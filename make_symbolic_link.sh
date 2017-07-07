91f7958d1994e1658398dd5b4c15cf61db1076ce
Naoya Yamaguchi <708yamaguchi@gmail.com>
2017-07-07 12:49:18 +0900

    add uim config

--- make_symbolic_link.sh
+++ make_symbolic_link.sh
@@ -4,9 +4,11 @@ eval ln -sfn  $dotfiles_path/bash/bashrc ~/.bashrc
 eval ln -sfn  $dotfiles_path/bash/bashrc.orig ~/.bashrc.orig
 eval ln -sfn $dotfiles_path/bash/bashrc.alias ~/.bashrc.alias
 eval ln -sf  $dotfiles_path/gitconfig ~/.gitconfig
+eval rm -rf ~/.percol.d
 eval mkdir ~/.percol.d
 eval ln -sfn $dotfiles_path/percol.d/rc.py ~/.percol.d/rc.py
 eval ln -sf  $dotfiles_path/tmux.conf ~/.tmux.conf
 eval ln -sf  $dotfiles_path/emacs ~/.emacs
 eval ln -sf  $dotfiles_path/profile ~/.profile
 eval ln -sf $dotfiles_path/Xmodmap ~/.Xmodmap
+eval ln -sf $dotfiles_path/xinputrc ~/.xinputrc
