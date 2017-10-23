cec3c50717f44b3fc2c1ee74cb80620fb20252cb
Naoya Yamaguchi <708yamaguchi@gmail.com>
2017-10-23 16:03:47 +0900

    delete old dotfiles before making symbolic links

--- make_symbolic_link.sh
+++ make_symbolic_link.sh
@@ -1,13 +1,19 @@
 # symbolic link
 dotfiles_path=$HOME/dotfiles
+eval rm -f ~/.bashrc
 eval ln -sfn  $dotfiles_path/bash/bashrc ~/.bashrc
+eval rm -f ~/.bashrc.orig
 eval ln -sfn  $dotfiles_path/bash/bashrc.orig ~/.bashrc.orig
+eval rm -f ~/.bashrc.alias
 eval ln -sfn $dotfiles_path/bash/bashrc.alias ~/.bashrc.alias
+eval rm -f ~/.gitconfig
 eval ln -sf  $dotfiles_path/gitconfig ~/.gitconfig
 eval rm -rf ~/.percol.d
 eval mkdir ~/.percol.d
 eval ln -sfn $dotfiles_path/percol.d/rc.py ~/.percol.d/rc.py
+eval rm -f ~/.tmux.conf
 eval ln -sf  $dotfiles_path/tmux.conf ~/.tmux.conf
+eval rm -f ~/.emacs
 eval ln -sf  $dotfiles_path/emacs ~/.emacs
 eval rm -rf ~/.profile
 eval ln -sf  $dotfiles_path/profile ~/.profile
