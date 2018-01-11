3ea0db99d1a8ff05863321e8eabc1552e3262de5
Naoya Yamaguchi <708yamaguchi@gmail.com>
2018-01-11 15:32:17 +0900

    add scripts for tmux2.5 & enable mouse mode

---
+++ install_scripts/install_tmux2.5.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+if [ "$(uname)" != "Linux" ]; then
+  exit 1
+fi
+
+if [ -f ~/.local/bin/tmux ]; then
+  exit 0
+fi
+
+sudo -H apt-get install -qq -y libevent-dev libncurses-dev
+
+TMPDIR=$(mktemp -d)
+cd $TMPDIR
+
+VERSION=2.5
+wget -q https://github.com/tmux/tmux/releases/download/${VERSION}/tmux-${VERSION}.tar.gz
+tar zxf tmux-${VERSION}.tar.gz
+cd tmux-${VERSION}
+
+./configure --prefix=$HOME/.local
+make -j
+make install
+
+rm -rf $TMPDIR
