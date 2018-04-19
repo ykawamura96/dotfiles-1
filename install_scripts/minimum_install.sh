6329c5d958d6a7f109bac2d2c2476d6d52ed858f
Naoya Yamaguchi <708yamaguchi@gmail.com>
2018-04-19 20:53:36 +0900

    add install script for apt and pip

---
+++ install_scripts/minimum_install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# apt-get install
+apt-get install apt
+apt-get install aptitude
+apt-get install emacs
+apt-get install ibus-mozc
+apt-get install indicator-multiload
+apt-get install git
+apt-get install htop
+apt-get install ntp
+apt-get install python-pip
+apt-get install ssh
+apt-get install subversion
+apt-get install wget
+
+# pip install
+pip install --upgrade pip
+pip install matplotlib
+pip install numpy
+pip install percol
