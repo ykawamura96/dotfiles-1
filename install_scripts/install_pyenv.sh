0d2667d886bad32f9a57eec5be62e1d89fab7039
Naoya Yamaguchi <708yamaguchi@gmail.com>
2018-05-16 17:16:12 +0900

    set chmod u+x to install scripts

--- install_scripts/install_pyenv.sh
+++
@@ -1,4 +0,0 @@
-curl -L https://raw.githubusercontent.com/pyenv/pyenv-installer/master/bin/pyenv-installer | bash
-echo 'export PATH="$HOME/.pyenv/bin:$PATH"' >> ~/.bashrc
-echo 'eval "$(pyenv init -)"' >> ~/.bashrc
-echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.bashrc
