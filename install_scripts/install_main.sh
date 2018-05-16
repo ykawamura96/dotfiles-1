9916aa82c067b194988ec12fbf552864fce8347a
Naoya Yamaguchi <708yamaguchi@gmail.com>
2018-05-16 17:42:04 +0900

    make install_main.sh more stylish

--- install_scripts/install_main.sh
+++ install_scripts/install_main.sh
@@ -1,27 +1,37 @@
 #!/bin/bash
 
 # apt-get install
-apt-get -y install apt
-apt-get -y install aptitude
-apt-get -y install emacs
-apt-get -y install ibus-mozc
-apt-get -y install indicator-multiload
-apt-get -y install ipython
-apt-get -y install git
-apt-get -y install gnome-tweak-tool
-apt-get -y install htop
-apt-get -y install ntp
-apt-get -y install python-pip
-apt-get -y install ssh
-apt-get -y install subversion
-apt-get -y install wget
+apt_packages=(
+    apt
+    aptitude
+    emacs
+    ibus-mozc
+    indicator-multiload
+    ipython
+    git
+    gnome-tweak-tool
+    htop
+    ntp
+    python-pip
+    ssh
+    subversion
+    wget
+)
 
+for apt_package in ${apt_packages[@]}; do
+    apt-get -y install $apt_package;
+done
 
 # pip install
 pip install --upgrade pip
-pip install matplotlib
-pip install numpy
-pip install percol
+pip_packages=(
+    matplotlib
+    numpy
+    percol
+)
+for pip_package in ${pip_packages[@]}; do
+    apt-get -y install $pip_package;
+done
 
 # from web source
 curl -L https://raw.githubusercontent.com/pyenv/pyenv-installer/master/bin/pyenv-installer | bash
