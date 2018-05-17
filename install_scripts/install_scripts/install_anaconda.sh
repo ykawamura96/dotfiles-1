a948050d0dea8b1d88a75274462f68b402eb0d71
Naoya Yamaguchi <708yamaguchi@gmail.com>
2018-05-18 01:32:22 +0900

    change how to install anaconda

--- install_scripts/install_scripts/install_anaconda.sh
+++ install_scripts/install_scripts/install_anaconda.sh
@@ -1,8 +1,13 @@
 # from web source
-curl -L https://raw.githubusercontent.com/pyenv/pyenv-installer/master/bin/pyenv-installer | bash
-export PATH="$HOME/.pyenv/bin:$PATH"
+# for ubuntu 14.04?
+# curl -L https://raw.githubusercontent.com/pyenv/pyenv-installer/master/bin/pyenv-installer | bash
+# for ubuntu 16.04?
+git clone https://github.com/yyuu/pyenv.git ~/.pyenv
+git clone https://github.com/yyuu/pyenv-pip-rehash.git ~/.pyenv/plugins/pyenv-pip-rehash
+
+export PYENV_ROOT=$HOME/.pyenv
+export PATH=$PYENV_ROOT/bin:$PATH
 eval "$(pyenv init -)"
-eval "$(pyenv virtualenv-init -)"
 pyenv install anaconda3-4.3.1
-source activate anaconda3-4.3.1
+pyenv global anaconda3-4.3.1
 conda create -n python3.5 python=3.5 anaconda
