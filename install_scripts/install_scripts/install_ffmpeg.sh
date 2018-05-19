6227a6144c1e3434d6fc61b695c7819cc412b50a
Naoya Yamaguchi <708yamaguchi@gmail.com>
2018-05-19 17:26:32 +0900

    add -y option when installing ffmpeg

--- install_scripts/install_scripts/install_ffmpeg.sh
+++ install_scripts/install_scripts/install_ffmpeg.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 sudo apt-get -y install software-properties-common
-sudo add-apt-repository ppa:mc3man/trusty-media
+sudo add-apt-repository ppa:mc3man/trusty-media -y
 sudo apt-get install update
 sudo apt-get -y install ffmpeg
