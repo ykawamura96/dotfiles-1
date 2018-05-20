689d9dd712496c98377f3dc6e2493373bcd56ad2
Naoya Yamaguchi <708yamaguchi@gmail.com>
2018-05-20 09:02:26 +0900

    change how to create symbolic links of dot files (enable directory structure)

--- install_scripts/make_symbolic_link.sh
+++ install_scripts/make_symbolic_link.sh
@@ -1,27 +1,24 @@
 # symbolic link
 dotfiles_path=$HOME/dotfiles
-eval rm -f ~/.bashrc
-eval ln -sfn  $dotfiles_path/bash/bashrc ~/.bashrc
-eval rm -f ~/.bashrc.orig
-eval ln -sfn  $dotfiles_path/bash/bashrc.orig ~/.bashrc.orig
-eval rm -f ~/.bashrc.alias
-eval ln -sfn $dotfiles_path/bash/bashrc.alias ~/.bashrc.alias
-eval rm -f ~/.gitconfig
-eval ln -sf  $dotfiles_path/gitconfig ~/.gitconfig
-eval rm -rf ~/.percol.d
-eval mkdir ~/.percol.d
-eval ln -sfn $dotfiles_path/percol.d/rc.py ~/.percol.d/rc.py
-eval rm -f ~/.tmux.conf
-eval ln -sf  $dotfiles_path/tmux.conf ~/.tmux.conf
-eval rm -f ~/.emacs
-eval ln -sf  $dotfiles_path/emacs ~/.emacs
-eval rm -rf ~/.profile
-eval ln -sf  $dotfiles_path/profile ~/.profile
-eval rm -rf ~/.Xmodmap
-eval ln -sf $dotfiles_path/Xmodmap ~/.Xmodmap
-# eval rm -rf ~/.xinputrc
-# eval ln -sf $dotfiles_path/xinputrc ~/.xinputrc
-# eval rm -rf ~/.uim.d
-# eval ln -sf $dotfiles_path/uim.d ~/.uim.d
-eval rm -rf ~/.condarc
-eval ln -sf $dotfiles_path/condarc ~/.condarc
+symbolic_links=(
+    bashrc
+    bashrc.alias
+    bashrc.orig
+    condarc
+    emacs
+    gitconfig
+    percol.d/rc.py
+    profile
+    tmux.conf
+    # uim.d
+    # xinputrc
+    Xmodmap
+)
+
+for symbolic in ${symbolic_links[@]}; do
+    eval rm -rf ~/"."$(echo $symbolic | awk -F'[/]' '{print $1}')
+    if [ $(echo $symbolic | awk -F'[/]' '{print $2}') ]; then
+        eval mkdir ~/"."${symbolic%$(echo $symbolic | awk -F'[/]' '{print $NF}')}
+    fi
+    eval ln -sfn $dotfiles_path/$symbolic ~/"."$symbolic
+done
