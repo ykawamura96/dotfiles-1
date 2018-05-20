ffd8947afdaac3c0f45ddcb50418f9ef990ddd17
Naoya Yamaguchi <708yamaguchi@gmail.com>
2018-05-20 10:17:03 +0900

    display progress of install_main.sh

--- install_scripts/install_main.sh
+++ install_scripts/install_main.sh
@@ -1,10 +1,25 @@
 #!/bin/bash
 
-# scripts
-./includes/install_apt.sh
-./includes/install_pip.sh
-./includes/setup.sh
-./includes/install_ffmpeg.sh
-./includes/install_tmux2.5.sh
-./includes/install_anaconda.sh
-./includes/install_mozc.sh
+# execute scripts
+scripts=(
+    install_apt.sh
+    install_pip.sh
+    setup.sh
+    install_ffmpeg.sh
+    install_tmux2.5.sh
+    install_anaconda.sh
+    install_mozc.sh
+)
+
+progress="";
+loop_count=1;
+for script in ${scripts[@]}; do
+    # ./includes/$script;
+    max_sharp=$(( $(tput cols) - 5))
+    script_num=${#scripts[@]}
+    for i in `seq $(expr $max_sharp / $script_num)`; do
+        progress=$progress"#";
+    done;
+    echo -e "\033[0;32m$progress""  $(expr $(expr 100 \* $loop_count) / $script_num)%\033[0;39m"
+    loop_count=$(( loop_count + 1 ))
+done;
