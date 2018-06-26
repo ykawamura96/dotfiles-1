85a3f35ca7c3137f9f7073bab1073fccb7dc0b7b
Naoya Yamaguchi <708yamaguchi@gmail.com>
2018-06-26 23:22:54 +0900

    add script to change speed and sensitivity of track point of Thinkpad

---
+++ others/speedup_trackpoint.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+sudo -s
+echo 'KERNEL=="serio2", SUBSYSTEM=="serio", DRIVERS=="psmouse", WAIT_FOR="/sys/devices/platform/i8042/serio1/serio2/sensitivity", ATTR{sensitivity}:="180", ATTR{speed}:="150"' > /etc/udev/rules.d/10-trackpoint.rules
