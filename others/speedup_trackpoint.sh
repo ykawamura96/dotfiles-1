9d0c548336d74b10469320ecd43c9117a25a0af4
Naoya Yamaguchi <708yamaguchi@gmail.com>
2018-06-27 14:48:23 +0900

    add comment to speedup_trackpoint.sh

--- others/speedup_trackpoint.sh
+++ others/speedup_trackpoint.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+# before setting 'sensitivity' and 'speed', check the parameters with commands like below:
+# sudo -s
+# echo -n 180 > /sys/devices//platform/i8042/serio1/serio2/sensitivity
+# echo -n 150 > /sys/devices//platform/i8042/serio1/serio2/speed
+
+
 sudo -s
 echo 'KERNEL=="serio2", SUBSYSTEM=="serio", DRIVERS=="psmouse", WAIT_FOR="/sys/devices/platform/i8042/serio1/serio2/sensitivity", ATTR{sensitivity}:="180", ATTR{speed}:="150"' > /etc/udev/rules.d/10-trackpoint.rules
 exit
