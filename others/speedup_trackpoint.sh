e5731528415c5a8de607fb81d6e0e28ed8c6a4fc
Naoya Yamaguchi <708yamaguchi@gmail.com>
2018-10-28 16:40:02 +0900

    fix bug in speedup_trackpoint.sh

--- others/speedup_trackpoint.sh
+++ others/speedup_trackpoint.sh
@@ -5,7 +5,4 @@
 # echo -n 180 > /sys/devices//platform/i8042/serio1/serio2/sensitivity
 # echo -n 150 > /sys/devices//platform/i8042/serio1/serio2/speed
 
-
-sudo -s
-echo 'KERNEL=="serio2", SUBSYSTEM=="serio", DRIVERS=="psmouse", WAIT_FOR="/sys/devices/platform/i8042/serio1/serio2/sensitivity", ATTR{sensitivity}:="70", ATTR{speed}:="100"' > /etc/udev/rules.d/10-trackpoint.rules
-exit
+sudo sh -c "echo 'KERNEL=="serio2", SUBSYSTEM=="serio", DRIVERS=="psmouse", WAIT_FOR="/sys/devices/platform/i8042/serio1/serio2/sensitivity", ATTR{sensitivity}:="50", ATTR{speed}:="150"' > /etc/udev/rules.d/10-trackpoint.rules"
