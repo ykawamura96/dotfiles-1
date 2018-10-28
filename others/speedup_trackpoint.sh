5e59fc95e12f60eb430e3e63d14e37622820dc36
Naoya Yamaguchi <708yamaguchi@gmail.com>
2018-10-28 16:09:37 +0900

    change sensitivity and speed of trackpoint

--- others/speedup_trackpoint.sh
+++ others/speedup_trackpoint.sh
@@ -7,5 +7,5 @@
 
 
 sudo -s
-echo 'KERNEL=="serio2", SUBSYSTEM=="serio", DRIVERS=="psmouse", WAIT_FOR="/sys/devices/platform/i8042/serio1/serio2/sensitivity", ATTR{sensitivity}:="180", ATTR{speed}:="150"' > /etc/udev/rules.d/10-trackpoint.rules
+echo 'KERNEL=="serio2", SUBSYSTEM=="serio", DRIVERS=="psmouse", WAIT_FOR="/sys/devices/platform/i8042/serio1/serio2/sensitivity", ATTR{sensitivity}:="70", ATTR{speed}:="100"' > /etc/udev/rules.d/10-trackpoint.rules
 exit
